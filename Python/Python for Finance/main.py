c4667028c0270bf7f6817249b65d5e8fafc20e5c
Kyle Destura <kyle.afundar@gmail.com>
2022-12-28 22:26:55 +0800

    Add Coral Indicator

--- Python/Python for Finance/main.py
+++ Python/Python for Finance/main.py
@@ -44,37 +44,55 @@ def main():
     close_price = ohlc_data['ClosePrice']
     hl2_price = hl2(ohlc_data)
 
+    # Exponential Moving Average (EMA)
     ema_length = 100
     ema_values = ema(close_price, ema_length)
-    ohlc_data = ohlc_data.assign(EMA=pd.Series(ema_values, index=ohlc_data.index))
-    ema_values = ohlc_data['EMA']
+    ohlc_data = ohlc_data.assign(EMA=ema_values)
 
+    # True Strength Index (TSI)
     tsi_slow = 12
     tsi_fast = 26
     tsi_smooth = 19
     tsi_values, tsi_signal = tsi_mod(hl2_price, tsi_slow, tsi_fast, tsi_smooth)
-    ohlc_data = ohlc_data.assign(TSI=pd.Series(tsi_values, index=ohlc_data.index))
-    ohlc_data = ohlc_data.assign(TSISignal=pd.Series(tsi_signal, index=ohlc_data.index))
-    tsi_values = ohlc_data['TSI']
-    tsi_signal = ohlc_data['TSISignal']
+    ohlc_data = ohlc_data.assign(TSI=tsi_values)
+    ohlc_data = ohlc_data.assign(TSISignal=tsi_signal)
 
+    # Parabolic SAR
     psar_increment = 0.06
     psar_maximum = 0.035
-    psar_up, psar_down = psar(high_price, low_price, close_price, psar_increment, psar_maximum)
+    sar_up, sar_down = psar(high_price, low_price, close_price, psar_increment, psar_maximum)
+    ohlc_data = ohlc_data.assign(PSARUP=sar_up)
+    ohlc_data = ohlc_data.assign(PSARDOWN=sar_down)
 
+    # Coral
+    coral_length = 20
+    coral_multiplier = 0.7
+    coral_up, coral_down = coral(close_price, coral_length, coral_multiplier)
+    ohlc_data = ohlc_data.assign(CORALUP=coral_up)
+    ohlc_data = ohlc_data.assign(CORALDOWN=coral_down)
+
+    # Display Indicators
     fig = plt.figure()
 
-    ax1 = fig.add_subplot(211, ylabel='Price')
+    # Price + EMA + PSAR Plot
+    ax1 = fig.add_subplot(311, ylabel='Price')
     ax1.set_facecolor('xkcd:black')
     close_price.plot(ax=ax1, color='green', lw=0.7, legend=True)
     ema_values.plot(ax=ax1, color='b', lw=0.7, legend=True)
-    psar_up.plot(ax=ax1, color='green', lw=0.7, legend=True)
-    psar_down.plot(ax=ax1, color='red', lw=0.7, legend=True)
+    sar_up.plot(ax=ax1, color='green', lw=0.7, legend=True)
+    sar_down.plot(ax=ax1, color='red', lw=0.7, legend=True)
 
-    ax2 = fig.add_subplot(212, ylabel='TSI')
+    # TSI Plot
+    ax2 = fig.add_subplot(312, ylabel='TSI')
     ax2.set_facecolor('xkcd:black')
     tsi_values.plot(ax=ax2, color='green', lw=0.7, legend=True)
     tsi_signal.plot(ax=ax2, color='red', lw=0.7, legend=True)
+
+    # Coral Plot
+    ax3 = fig.add_subplot(313, ylabel='Coral')
+    ax3.set_facecolor('xkcd:black')
+    coral_up.plot(ax=ax3, color='green', lw=0.7, legend=True)
+    coral_down.plot(ax=ax3, color='red', lw=0.7, legend=True)
     plt.show()
 
 
--- Python/Python for Finance/test/indicators/ta.py
+++ Python/Python for Finance/test/indicators/ta.py
@@ -1,8 +1,24 @@
 """ Exponential Moving Averages """
 
-import pandas as pd
 from ta.trend import *
 from ta.momentum import *
+
+
+def nz(x, y=None):
+    """
+    RETURNS
+    Two args version: returns x if it's a valid (not NaN) number, otherwise y
+    One arg version: returns x if it's a valid (not NaN) number, otherwise 0
+    ARGUMENTS
+    x (val) Series of values to process.
+    y (float) Value that will be inserted instead of all NaN values in x series.
+    """
+
+    if x != x:
+        if y is not None:
+            return y
+        return 0
+    return x
 
 
 def ema(source, period):
@@ -15,6 +31,7 @@ def sma(source, length):
 
 def tsi_mod(source, slow=12, fast=26, signal=19):
     """ True Strength Index Modified
+
     Returns rounded TSI value
     Returns TSI Signal Value
     """
@@ -24,9 +41,7 @@ def tsi_mod(source, slow=12, fast=26, signal=19):
 
     i = 0
     while i < len(tsi_value):
-
         difference = abs(tsi_value[i] - tsi_signal[i])
-
         if difference <= 7:
             tsi_value[i] = tsi_signal[i]
             tsi_signal[i] = tsi_value[i]
@@ -36,6 +51,57 @@ def tsi_mod(source, slow=12, fast=26, signal=19):
 
 
 def psar(high, low, close, step, max_step):
+    """Parabolic SAR
+
+    Returns Psar Up and Psar Down
+    """
     up = psar_up(high, low, close, step, max_step)
     down = psar_down(high, low, close, step, max_step)
     return up, down
+
+
+def coral(source, sm, cd):
+    source_len = len(source)
+    i1 = [0] * source_len
+    i2 = [0] * source_len
+    i3 = [0] * source_len
+    i4 = [0] * source_len
+    i5 = [0] * source_len
+    i6 = [0] * source_len
+
+    di = (sm - 1.0) / 2.0 + 1.0
+    c1 = 2 / (di + 1.0)
+    c2 = 1 - c1
+    c3 = 3.0 * (cd * cd + cd * cd * cd)
+    c4 = -3.0 * (2.0 * cd * cd + cd + cd * cd * cd)
+    c5 = 3.0 * cd + 1.0 + cd * cd * cd + 3.0 * cd * cd
+
+    bfr = [] * source_len
+    bfr_up = [] * source_len
+    bfr_down = [] * source_len
+
+    i = 1
+    while i < source_len:
+
+        i1[i] = c1 * source[i] + c2 * nz(i1[i-1])
+        i2[i] = c1 * i1[i] + c2 * nz(i2[i-1])
+        i3[i] = c1 * i2[i] + c2 * nz(i3[i-1])
+        i4[i] = c1 * i3[i] + c2 * nz(i4[i-1])
+        i5[i] = c1 * i4[i] + c2 * nz(i5[i-1])
+        i6[i] = c1 * i5[i] + c2 * nz(i6[i-1])
+
+        bfr.append(-cd * cd * cd * i6[i] + (c3*i5[i]) + (c4*i4[i]) + (c5*i3[i]))
+
+        if bfr[i-1] > nz(bfr[i-2]):
+            bfr_up.append(bfr[i-1])
+            bfr_down.append(None)
+        else:
+            bfr_down.append(bfr[i-1])
+            bfr_up.append(None)
+
+        i += 1
+
+    bfr_up = pd.Series(bfr_up)
+    bfr_down = pd.Series(bfr_down)
+
+    return bfr_up, bfr_down
