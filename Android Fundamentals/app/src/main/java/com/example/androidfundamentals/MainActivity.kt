345a697a3652ceb777c6ef18570e1db2a8267899
Kyle Destura <kyle.afundar@gmail.com>
2023-02-04 16:05:24 +0800

    Add Android Fundamentals

--- Android Fundamentals/app/src/main/java/com/example/androidfundamentals/MainActivity.kt
+++ Android Fundamentals/app/src/main/java/com/example/androidfundamentals/MainActivity.kt
@@ -4,8 +4,21 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    //Overriding life cycle of activity
+    // onCreate() -> onStart() -> onResume()
+    // onPause() -> onStop()
+    // onDestroy() -> onRestart()
+
+    //sample implementation of overriding lifecycle
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+
 }
