e26599933e6bbd913ab15af9adae61153f4b4f7d
Kyle Destura <kyle.afundar@gmail.com>
2023-02-27 21:15:02 +0800

    update

--- Android Fundamentals/app/src/main/java/com/example/androidfundamentals/SampleRoom.kt
+++ Android Fundamentals/app/src/main/java/com/example/androidfundamentals/SampleRoom.kt
@@ -3,7 +3,6 @@ package com.example.androidfundamentals
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.NavController
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.androidfundamentals.databinding.ActivitySampleRoomBinding
