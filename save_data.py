db415122b0e18cdc93afd587c688ec1249b7abc9
Kyle Destura <kyle.afundar@gmail.com>
2023-05-17 10:43:31 +0800

    Restructure saving of data. Add monthly saving. Change directory logic

--- save_data.py
+++ save_data.py
@@ -4,10 +4,10 @@ from mt5.connect import *
 from datetime import datetime
 import pandas as pd
 from pathlib import Path
+import calendar
 
 
 def get_timeframe_string(timeframe):
-
     if timeframe == mt5.TIMEFRAME_M1:
         return 'M1'
     elif timeframe == mt5.TIMEFRAME_M5:
@@ -24,36 +24,70 @@ def get_timeframe_string(timeframe):
         return 'D1'
 
 
-def save_data_yearly(account_info, start_year, end_year, ticker_name, timeframe):
+def is_directory_exist(path):
+
+    directory = Path(path)
+
+    if not directory.exists():
+        directory.mkdir(parents=True)
+
+    if directory.exists():  # Double check
+        return True
+
+
+def save_data(account_info, ticker_name, timeframe,
+              start_year, end_year,
+              monthly=False, daily=False):
 
     connect_to_mt(account_info[0], account_info[1], account_info[2])
 
-    for i in range((end_year-start_year) + 1):
+    for i in range((end_year - start_year) + 1):
         year = start_year + i
-        start_date = datetime(year, 1, 1)
-        end_date = datetime(year, 12, 31)
-        df = pd.DataFrame(mt5.copy_rates_range(ticker_name, timeframe, start_date, end_date))
         timeframe_string = get_timeframe_string(timeframe)
-        directory = Path('./data/' + ticker_name + '/yearly/' + timeframe_string)
 
-        if not directory.exists():
-            directory.mkdir(parents=True)
-        else:
-            df.to_csv('./data/' + ticker_name + '/yearly/' + timeframe_string + '/' + str(year) + '.csv')
+        # Record Yearly
+        if (not monthly) & (not daily):
 
+            path = './data/' + ticker_name + '/' + timeframe_string + '/yearly'
 
-def save_data_monthly(start_year, end_year, start_month, end_month, ticker_name, timeframe):
-    directory = Path('./ticker_name/monthly/' + str(timeframe))
+            if is_directory_exist(path):
+                start_date = datetime(year, 1, 1)
+                end_date = datetime(year, 12, 31)
+                df = pd.DataFrame(mt5.copy_rates_range(ticker_name, timeframe, start_date, end_date))
+                df.to_csv('./data/'
+                          + ticker_name
+                          + '/'
+                          + timeframe_string
+                          + '/yearly/'
+                          + str(year)
+                          + '.csv')
 
-    if not directory.exists():
-        directory.mkdir(parents=True)
+        # Record monthly
+        if monthly:
 
+            path = './data/' + ticker_name + '/' + timeframe_string + '/monthly/' + str(year)
 
-def save_data_daily(start_year, end_year, start_month, end_month, start_day, end_day, ticker_name, timeframe):
-    directory = Path('./ticker_name/daily/' + str(timeframe))
+            if is_directory_exist(path):
+                for j in range(1, 13):
+                    num_days = calendar.monthrange(year, j)[1]  # Take number of days each month
+                    start_date = datetime(year, j, 1)
+                    end_date = datetime(year, j, num_days)
 
-    if not directory.exists():
-        directory.mkdir(parents=True)
+                    df = pd.DataFrame(mt5.copy_rates_range(ticker_name, timeframe, start_date, end_date))
+                    df.to_csv('./data/'
+                              + ticker_name
+                              + '/'
+                              + timeframe_string
+                              + '/monthly/'
+                              + str(year)
+                              + '/'
+                              + str(j)
+                              + '.csv')
+
+        # Record daily
+        if daily:
+            for j in range(12):
+                num_days = calendar.monthrange(year, j)[i]
 
 
 def main():
@@ -65,7 +99,8 @@ def main():
     ticker_name = 'EURJPY#'
     ticker_timeframe = mt5.TIMEFRAME_H1
 
-    save_data_yearly(account_info, 2021, 2023, ticker_name, ticker_timeframe)
+    save_data(account_info, ticker_name, ticker_timeframe,
+              2021, 2023, monthly=True)
 
 
 main()
