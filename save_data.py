edcb132ccb71eb8f14b2654a6460c08b9347b8f9
Kyle Destura <kyle.afundar@gmail.com>
2023-05-17 16:02:29 +0800

    Add saving daily data. Add more security

--- save_data.py
+++ save_data.py
@@ -25,7 +25,6 @@ def get_timeframe_string(timeframe):
 
 
 def is_directory_exist(path):
-
     directory = Path(path)
 
     if not directory.exists():
@@ -37,12 +36,12 @@ def is_directory_exist(path):
 
 def save_data(account_info, ticker_name, timeframe,
               start_year, end_year,
+              start_month=0, end_month=0,
               monthly=False, daily=False):
-
     connect_to_mt(account_info[0], account_info[1], account_info[2])
 
-    for i in range((end_year - start_year) + 1):
-        year = start_year + i
+    for year in range(start_year, end_year+1):
+
         timeframe_string = get_timeframe_string(timeframe)
 
         # Record Yearly
@@ -54,13 +53,15 @@ def save_data(account_info, ticker_name, timeframe,
                 start_date = datetime(year, 1, 1)
                 end_date = datetime(year, 12, 31)
                 df = pd.DataFrame(mt5.copy_rates_range(ticker_name, timeframe, start_date, end_date))
-                df.to_csv('./data/'
-                          + ticker_name
-                          + '/'
-                          + timeframe_string
-                          + '/yearly/'
-                          + str(year)
-                          + '.csv')
+
+                if len(df) > 5:  # Save only if df contains data
+                    df.to_csv('./data/'
+                              + ticker_name
+                              + '/'
+                              + timeframe_string
+                              + '/yearly/'
+                              + str(year)
+                              + '.csv')
 
         # Record monthly
         if monthly:
@@ -74,20 +75,46 @@ def save_data(account_info, ticker_name, timeframe,
                     end_date = datetime(year, j, num_days)
 
                     df = pd.DataFrame(mt5.copy_rates_range(ticker_name, timeframe, start_date, end_date))
-                    df.to_csv('./data/'
-                              + ticker_name
-                              + '/'
-                              + timeframe_string
-                              + '/monthly/'
-                              + str(year)
-                              + '/'
-                              + str(j)
-                              + '.csv')
+
+                    if len(df) > 5:  # Save only if df contains data
+                        df.to_csv('./data/'
+                                  + ticker_name
+                                  + '/'
+                                  + timeframe_string
+                                  + '/monthly/'
+                                  + str(year)
+                                  + '/'
+                                  + str(j)
+                                  + '.csv')
 
         # Record daily
         if daily:
-            for j in range(12):
-                num_days = calendar.monthrange(year, j)[i]
+            if start_month > 0:
+
+                for month in range(start_month, end_month + 1):
+
+                    path = './data/' + ticker_name + '/' + timeframe_string + '/daily/' + str(year) + '/' + str(month) + '/'
+
+                    if is_directory_exist(path):
+                        num_days = calendar.monthrange(year, month)[1]  # Take number of days each month
+
+                        for day in range(1, num_days):
+                            start_date = datetime(year, month, day)
+                            end_date = datetime(year, month, day+1)
+                            df = pd.DataFrame(mt5.copy_rates_range(ticker_name, timeframe, start_date, end_date))
+
+                            if len(df) > 5:   # Save only if df contains data
+                                df.to_csv('./data/'
+                                          + ticker_name
+                                          + '/'
+                                          + timeframe_string
+                                          + '/daily/'
+                                          + str(year)
+                                          + '/'
+                                          + str(month)
+                                          + '/'
+                                          + str(day)
+                                          + '.csv')
 
 
 def main():
@@ -97,10 +124,12 @@ def main():
     account_info = [login, password, server]
 
     ticker_name = 'EURJPY#'
-    ticker_timeframe = mt5.TIMEFRAME_H1
+    ticker_timeframe = mt5.TIMEFRAME_M1
 
     save_data(account_info, ticker_name, ticker_timeframe,
-              2021, 2023, monthly=True)
+              start_year=2023, end_year=2023,
+              start_month=2, end_month=5,
+              daily=True)
 
 
 main()
