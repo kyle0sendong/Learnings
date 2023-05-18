8052e4333e0df2472471006a7969845ba1ac7577
Kyle Destura <kyle.afundar@gmail.com>
2023-05-18 12:16:02 +0800

    Change saving of file logic. Only create after confirming DataFrame has data

--- save_data.py
+++ save_data.py
@@ -5,11 +5,14 @@ from datetime import datetime
 import pandas as pd
 from pathlib import Path
 import calendar
+import shutil
 
 
 def get_timeframe_string(timeframe):
     if timeframe == mt5.TIMEFRAME_M1:
         return 'M1'
+    elif timeframe == mt5.TIMEFRAME_M3:
+        return 'M3'
     elif timeframe == mt5.TIMEFRAME_M5:
         return 'M5'
     elif timeframe == mt5.TIMEFRAME_M15:
@@ -49,12 +52,12 @@ def save_data(account_info, ticker_name, timeframe,
 
             path = './data/' + ticker_name + '/' + timeframe_string + '/yearly'
 
-            if is_directory_exist(path):
-                start_date = datetime(year, 1, 1)
-                end_date = datetime(year, 12, 31)
-                df = pd.DataFrame(mt5.copy_rates_range(ticker_name, timeframe, start_date, end_date))
+            start_date = datetime(year, 1, 1)
+            end_date = datetime(year, 12, 31)
+            df = pd.DataFrame(mt5.copy_rates_range(ticker_name, timeframe, start_date, end_date))
 
-                if len(df) > 5:  # Save only if df contains data
+            if len(df) > 5:  # Save only if df contains data
+                if is_directory_exist(path):
                     df.to_csv('./data/'
                               + ticker_name
                               + '/'
@@ -68,15 +71,15 @@ def save_data(account_info, ticker_name, timeframe,
 
             path = './data/' + ticker_name + '/' + timeframe_string + '/monthly/' + str(year)
 
-            if is_directory_exist(path):
-                for j in range(1, 13):
-                    num_days = calendar.monthrange(year, j)[1]  # Take number of days each month
-                    start_date = datetime(year, j, 1)
-                    end_date = datetime(year, j, num_days)
+            for j in range(1, 13):
+                num_days = calendar.monthrange(year, j)[1]  # Take number of days each month
+                start_date = datetime(year, j, 1)
+                end_date = datetime(year, j, num_days)
 
-                    df = pd.DataFrame(mt5.copy_rates_range(ticker_name, timeframe, start_date, end_date))
+                df = pd.DataFrame(mt5.copy_rates_range(ticker_name, timeframe, start_date, end_date))
 
-                    if len(df) > 5:  # Save only if df contains data
+                if len(df) > 5:  # Save only if df contains data
+                    if is_directory_exist(path):
                         df.to_csv('./data/'
                                   + ticker_name
                                   + '/'
@@ -93,17 +96,17 @@ def save_data(account_info, ticker_name, timeframe,
 
                 for month in range(start_month, end_month + 1):
 
-                    path = './data/' + ticker_name + '/' + timeframe_string + '/daily/' + str(year) + '/' + str(month) + '/'
+                    path = './data/' + ticker_name + '/' + timeframe_string + '/daily/' + str(year) + '/' + str(month)
 
-                    if is_directory_exist(path):
-                        num_days = calendar.monthrange(year, month)[1]  # Take number of days each month
+                    num_days = calendar.monthrange(year, month)[1]  # Take number of days each month
 
-                        for day in range(1, num_days):
-                            start_date = datetime(year, month, day)
-                            end_date = datetime(year, month, day+1)
-                            df = pd.DataFrame(mt5.copy_rates_range(ticker_name, timeframe, start_date, end_date))
+                    for day in range(1, num_days):
+                        start_date = datetime(year, month, day)
+                        end_date = datetime(year, month, day+1)
+                        df = pd.DataFrame(mt5.copy_rates_range(ticker_name, timeframe, start_date, end_date))
 
-                            if len(df) > 5:   # Save only if df contains data
+                        if len(df) > 5:   # Save only if df contains data
+                            if is_directory_exist(path):
                                 df.to_csv('./data/'
                                           + ticker_name
                                           + '/'
@@ -123,12 +126,12 @@ def main():
     server = 'XMGlobal-MT5 2'
     account_info = [login, password, server]
 
-    ticker_name = 'EURJPY#'
-    ticker_timeframe = mt5.TIMEFRAME_M1
+    ticker_name = 'EURUSD#'
+    ticker_timeframe = mt5.TIMEFRAME_M3
 
     save_data(account_info, ticker_name, ticker_timeframe,
-              start_year=2023, end_year=2023,
-              start_month=2, end_month=5,
+              start_year=2022, end_year=2023,
+              start_month=1, end_month=12,
               daily=True)
 
 
