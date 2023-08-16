f8ae39b7800d7d84d7405e548e7ab26cb4591981
Kyle Destura <kyle.afundar@gmail.com>
2023-08-16 16:19:05 +0800

    ReactJs States

---
+++ ReactJs/tutorial/src/ex4-states.js
@@ -0,0 +1,66 @@
+import "./App.css"
+import { useState } from "react";
+
+// hook - function that start with use
+
+function Ex4() {
+    
+    const [age, setAge] = useState(0);
+    const increaseAge = () => {
+        setAge(age + 1);
+    }
+
+    const [inputValue, setInputValue] = useState("")
+    
+    const handleInputChange = (event) => {
+        setInputValue(event.target.value);
+    }
+
+    const [showText, setShowText] = useState(true);
+
+    const[value, setValue] = useState(0);
+
+    const increaseValue = () => {
+        setValue(value+1)
+    }
+
+    const decreaseValue = () => {
+        setValue(value-1)
+    }
+
+    const setValueToZero = () => {
+        setValue(0)
+    }
+
+    return(
+        <div>
+            <h2>Example 4 - States</h2>
+            <div className="App">
+                <p>{age}</p>
+                <button onClick={increaseAge}>Increase Age!</button>
+
+                <div>
+                    <input type="text" onChange={handleInputChange} placeholder="Type text here!"></input>
+                    <p>{inputValue}</p>
+                </div>
+
+                <div>
+                    <button onClick={ () => {setShowText(!showText)}}>Show/Hide Text</button>
+                    {showText === true && <h3>Hello Hidden Text</h3>}
+                </div>
+
+            </div>
+
+            <h3>Example 4 exercise</h3>
+            <div className="App">
+                <h3>{value}</h3>
+                <button onClick={increaseValue}>Increase</button>
+                <button onClick={decreaseValue}>Decrease</button>
+                <button onClick={setValueToZero}>Set Zero</button>
+            </div>
+            <hr/>
+        </div>
+    );
+}
+
+export default Ex4;
